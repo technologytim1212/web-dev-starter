4e04773a58a4dd772a609ed3e1cd32db744d72d8
Tim Dunning <timunsungdjs@gmail.com>
2023-02-09 11:31:00 +1100

    Revert "Update home.js"
    This reverts commit da61924ac051f0ffa64aadb3ad538555a4992067.

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ let car = {
 
 manufacturer: 'Honda',
 colour: 'red',
-age: 45,
+
 }
 
 
@@ -11,11 +11,9 @@ function swapout(){
 
     car.manufacturer ='Jaguar';
     car.colour = 'black';
-    car.age = 23;
 }
 
 swapout();
 
 console.log(car.manufacturer);
 console.log(car.colour);
-console.log(car.age);
