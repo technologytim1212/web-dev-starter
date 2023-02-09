d4236467f4659b4327738987f05e4ef4c25c4b4b
Tim Dunning <timunsungdjs@gmail.com>
2023-02-09 11:59:38 +1100

    Add wheels

--- home.js
+++ home.js
@@ -3,6 +3,9 @@ let car = {
 
 manufacturer: 'Honda',
 colour: 'red',
+age: 20,
+wheels: 4
+
 }
 
 
@@ -10,9 +13,14 @@ function swapout(){
 
     car.manufacturer ='Jaguar';
     car.colour = 'black';
+    car.age = 30;
+
 }
 
 swapout();
 
 console.log(car.manufacturer);
 console.log(car.colour);
+console.log(car.age);
+console.log(car.wheels);
+
